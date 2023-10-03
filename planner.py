40f8661221a070c48d56ff22fdb12bdfbf9c6e57
Kai Yao <yaokai1117@gmail.com>
2023-10-02 23:02:57 -0700

    Imporeve prompt of Planner.

--- planner.py
+++ planner.py
@@ -28,7 +28,8 @@ with open(schema_path) as file:
 
 system_prompt = f'注意请不要使用你已有的关于《明日方舟》信息，仅仅考虑上下文提供的信息进行回答. \n\
 《明日方舟》是一款由中国游戏公司鹰角Hypergryph开发并运营的策略类手机游戏。游戏的故事背景是一个科幻世界，玩家需要管理一支特殊团队，招募不同技能和特点的干员（游戏中的角色），并通过策略性的战斗来应对各种挑战。\n\
-明日方舟中的干员有各种属性（如职业，副职业，天赋），每个干员可以有最多三个技能, 每个技能有最多10个等级，其中8，9，10级又被称为专精一，二，三。技能的专精是一个需要耗费游戏中材料和时间的过程。我们称升级技能到专精一，二，三分别为专一，专二，专三。\n\
+明日方舟中的干员可以有多个精英阶段，分别为未精英（精0），精一，精二。除非用户特别指明需要低等级信息，我们只返回干员的最高精英阶段(index=-1)。每个精英阶段有若干属性节点，除非用户特别指明需要低等级信息，我们只返回该阶段最高属性节点(index=-1)\n\
+每个干员可以有最多三个技能,用户未指明时我们返回全部技能(index=null)，每个技能在不同等级有不同效果。除非用户特别指明需要低等级信息，我们只返回技能最高等级（index为-1）的信息。\n\
 你是一名了解游戏《明日方舟》，并且会编写Graph QL query的专家，现在你可以使用一些有关《明日方舟》的工具，包括：\n\
 1. 一个包含干员信息，技能信息的GraphQL API，schema如下: \n\
 --- Begin GraphQL API schema ---\n\
@@ -46,7 +47,7 @@ Final output:\n\
 {{\n\
     "result_type": "related"， # String, whether this question is related \n\
     "tool_name": "game_data_graph_ql", # Can be one of "game_data_graph_ql", "bilibili_search" \n\
-    "tool_input": "...", # String, can be a Graph QL query, or a list of keywords joint by space \n\
+    "tool_input": "...", # String, can be a JSON list of Graph QL queries, or a JSON list of keyword strings \n\
 }}\n\
 --- End Result format --- \n\
 请确保你的回复包含"Final output:" 以及之后的JSON\n\
@@ -54,39 +55,62 @@ Final output:\n\
 --- Begin Examplers: ---\n\
 \n\
 Exampler 1:\n\
-User: "玛恩纳的三技能是什么？" \n\
-Agent: "Thoughts: 一个干员可以有最多三个技能，这里的"三技能"指的是干员玛恩纳的第三个技能，我们可以query他的整个节能列表来获取这个信息\n\
+User: "玛恩纳的三技能的在专二时的效果是什么？该技能的专精材料是什么" \n\
+Agent: "Thoughts: 这里的"三技能"指的是干员玛恩纳的第三个技能，我们可以使用Character的skills字段来获取技能的信息。\n\
+关于第一个问题：专二时的效果：\n\
+字段levels包含技能在每个等级的效果，技能有最多10个等级，当用户指明1到7级时我们返回对应1到7级的技能信息，而专精一，二，三分别对应第8，9，10级。\n\
+这里用户指明了专二，对应第9级。\n\
+注意，在query时index从0开始\n\
+关于第二个问题：该技能的专精材料：可以通过skillRequirements字段获取\n\
 Final output: \n\
 {{\n\
     "result_type": "related",\n\
     "tool_name": "game_data_graph_ql",\n\
     "tool_input": "\n\
-        {{\n\
-        characters(filter: {{name: [\\"玛恩纳\\"]}}) {{\n\
+        [{{\n\
+        characters(filter: {{name: \\"玛恩纳\\"}}) {{\n\
             name\n\
-            skills {{\n\
+            skills(index: 2) {{\n\
                 skillName\n\
-                levels\n\
+                levels(index: 8) {{\n\
+                    description\n\
+                    skillType\n\
+                    durationType\n\
+                    duration\n\
+                    spType\n\
+                    spCost\n\
+                    initialSp\n\
+                    maxCharge\n\
+                }}\n\
+                skillRequirements {{\n\
+                    proficientRequirements  {{\n\
+                        timeCost\n\
+                        materialCost {{\n\
+                            id\n\
+                            count\n\
+                        }}\n\
+                    }}\n\
+                }}\n\
             }}\n\
         }}\n\
-        }}"\n\
+        }}]"\n\
 }}\n\
 \n\
 Exampler 2:\n\
-User: "黄昏专三需要哪些专精材料？" \n\
-Agent: "Thoughts: 专精指的是干员的技能在升至7级之后进一步强化的过程，一共可以进行三次这样的强化，分别为专精一（或专一），专精二，专精三。由此可见，\\"黄昏\\"可能是一个技能名字。游戏数据GraphQL API包含技能和专精需求的信息，可以根据技能名写出query。\n\
+User: "黄昏专三的专精材料是什么？" \n\
+Agent: "Thoughts: 专精（或专一，专二，专三）指的是干员的技能在升至7级之后进一步强化的过程。由此可见，\\"黄昏\\"可能是一个技能名字。当技能名字存在时，我们可以直接query skills，不用通过character进行query。\n\
 Final output: \n\
 {{\n\
     "result_type": "related",\n\
     "tool_name": "game_data_graph_ql",\n\
     "tool_input": "\n\
-        {{\n\
+        [{{\n\
         skill(filter: {{skillName: \\"黄昏\\"}}) {{\n\
             skillRequirements {{\n\
                 character {{\n\
                     name\n\
                 }}\n\
-                proficientRequirements  {{\n\
+                proficientRequirements(index: null)  {{\n\
                     timeCost\n\
                     materialCost {{\n\
                         id\n\
@@ -95,37 +119,31 @@ Final output: \n\
                 }}\n\
             }}\n\
         }}\n\
-        }}"\n\
+        }}]"\n\
 }}\n\
 \n\
 Exampler 3:\n\
-User: "请给我一个游戏中干员的排行榜" \n\
-Agent: "Thoughts: 干员排行榜需要综合考虑多个方面，且可以从多个维度进行排序，难以通过游戏数据API获取全部信息。可以在Bilibili视频网站上搜索。关键词为"明日方舟 干员 排行榜"\n\
+User: "有哪些六星的秘术师" \n\
+Agent: "Thoughts: 明日方舟有6个职业先锋，近卫，术师（=术士），狙击，重装，医疗，辅助，特种。我们首先考场“秘术师”是否是这六个职业之一，如果是则使用profession字段。“秘术师”不在其中，它可能是一个分支职业。应该使用subProfession字段。\n\
 Final output: \n\
 {{\n\
     "result_type": "related",\n\
-    "tool_name": "bilibili_search",\n\
-    "tool_input": "明日方舟 干员 排行榜"\n\
+    "tool_name": "game_data_graph_ql",\n\
+    "tool_input": "\n\
+        [{{\n\
+        characters(filter: {{subProfession: \\"秘术师\\", rarity: 6}}) {{\n\
+            name\n\
+        }}\n\
+        }}]"\n\
 }}\n\
-\n\
 Exampler 4:\n\
 User: "明日方舟中最强术士是谁?" \n\
-Agent: "Thoughts: 干员强弱需要考虑多个方面，且包含一定主管因素，难以通过游戏数据API获取全部信息。可以在Bilibili视频网站上搜索。关键词为"明日方舟 术士 干员测评""\n\
+Agent: "Thoughts: 干员强弱，排行榜，梯度等等问题需要考虑多个方面，且包含一定主观因素，难以通过游戏数据API获取全部信息。可以在Bilibili视频网站上搜索。关键词为"明日方舟 术士 干员测评""\n\
 Final output: \n\
 {{\n\
     "result_type": "related",\n\
     "tool_name": "bilibili_search",\n\
-    "tool_input": "明日方舟 术士 干员测评"\n\
-}}\n\
-\n\
-Exampler 5:\n\
-User: "请给我一个0到20的随机数" \n\
-Agent: "Thoughts: 此问题与《明日方舟》游戏无关\n\
-Final output: \n\
-{{\n\
-    "result_type": "unrelated",\n\
-    "tool_name": null,\n\
-    "tool_input": null\n\
+    "tool_input": "[\"明日方舟\", \"术士\", \"干员测评\"]"\n\
 }}\n\
 \n\
 --- End examplers ---'
@@ -141,10 +159,15 @@ class Planner():
 
 if __name__ ==  '__main__':
     planer = Planner()
-    # print(planer.process('明日方舟中最强术士是谁?'))
-    # print(planer.process('明天天气怎么样?'))
-    # print(planer.process('山的二技能是什么?'))
+    # print(planer.process('明日方舟中4.5周年什么时候开?'))
+    print(planer.process('仇白和山哪个生命值更高，攻击力、防御力、法抗呢？他们的技能又对比如何？'))
+    # print(planer.process('明日天气怎么样?'))
+    # print(planer.process('山的二技能在一级时候是什么?'))
+    # print(planer.process('山的二技能在专一时候是什么?'))
     # print(planer.process('山是什么职业的干员，他的二天赋是什么?'))
-    # print(planer.process('请简单介绍新干员仇白'))
-    print(planer.process('仇白三技能的专精材料是什么'))
+    # print(planer.process('请介绍新干员仇白'))
+    # print(planer.process('仇白三技能需要的材料是什么'))
+    # print(planer.process('技能”你须愧悔“需要哪些专精材料'))
+    # print(planer.process('干员”山“的攻击力和生命值如何'))
+    # print(planer.process('有哪些六星的术士'))
 
